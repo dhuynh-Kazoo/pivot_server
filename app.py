d9ff52c85b5a8e3f74375f53d026e1fb3147b2e8
Dat Huynh <dhuynh@2600hz.com>
2024-03-12 09:28:18 +0000

    handle no action attribute and no recordingUrl attribute

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response, send_file, render_template, request
 import os
 import subprocess as sp
+import datetime
 
 app = Flask(__name__)
 
@@ -11,11 +12,7 @@ def hello_world():
 # http://localhost:5000/pivot/record/upload/call_recording_HiFzH3wlY9NwcWAw-J_W6Q...mp3
 @app.route("/pivot/record/upload/<string:recorded_file>", methods = ['POST', 'PUT'])
 def record_upload(recorded_file):
-    print("=========================================")
-    print("Request queries: ", request.args.to_dict())
-    print("Request Content-Type: ", request.content_type)
-    print("Request body: ", request.files)
-    print("=========================================")
+    logging_request(request)
 
     data = request.get_data()
     fname = os.path.join("./recorded_calls/" + recorded_file)
@@ -25,23 +22,40 @@ def record_upload(recorded_file):
 
 @app.route("/pivot/gather", methods = ["POST", "GET"])
 def collect_dtmf():
-    print("=========================================")
-    print("Request queries: ", request.args.to_dict())
-    json_data = request.form or request.get_json()
-    data = dict(json_data)
-    print("Request body: ", data)
-    print("=========================================")
+    logging_request(request)
     return '', 200    
 
 
-@app.route("/pivot/twiml/<string:action_file>")
-def pivot(action_file):
-    file_path = os.path.join(os.getcwd() + "/twiml/", action_file)
-    if os.path.exists(file_path):
-        out = sp.run(["php", file_path], stdout=sp.PIPE)
-        response = make_response(out.stdout)
-        response.headers["content-type"] = "text/xml;charset=UTF-8"
-        return response
-    else:
-        return "FILE NOT FOUND", 404
+@app.route("/pivot/twiml/<string:action_file>", methods=["POST", "GET"])
+@app.route("/pivot/twiml/<string:action_file>/<string:recorded_file>", methods=["PUT"])
+def pivot(action_file, recorded_file=None):
+    logging_request(request)
+    if request.method == "PUT": # no `action` and no `recordingUrl` attributes for `record` action
+        return '', 200
+    if request.method == "POST": # no `action` attribute
+        return '', 200
+    else: # GET method
+        file_path = os.path.join(os.getcwd() + "/twiml/", action_file)
+        if os.path.exists(file_path):
+            out = sp.run(["php", file_path], stdout=sp.PIPE)
+            response = make_response(out.stdout)
+            response.headers["content-type"] = "text/xml;charset=UTF-8"
+            return response
+        else:
+            return "FILE NOT FOUND", 404
 
+
+def logging_request(request):
+    now = datetime.datetime.now()
+    url = request.base_url
+    json_data = request.form or request.get_json()
+    if json_data:
+        data = dict(json_data)
+    else:
+        data = request.get_data()
+    
+    print("\n\n")
+    print("====================================")
+    print("[{}] - url {} {}".format(now, request.method, url))
+    print("[{}] - queries: {}".format(now, request.args.to_dict()))
+    print("[{}] - url {}".format(now, data))
